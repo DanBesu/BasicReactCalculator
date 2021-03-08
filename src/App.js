962702ecf9b13ba58dbc150414289137ee656155
911_Besu_Dan <danbesu15@gmail.com>
2021-03-08 20:36:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ function App() {
   return (
     
     <div className='tc'>
-      <h1 className='f1'> 
-        Here we have a Calculator
-      </h1>
+      <h1 className='f1'> Here we have a Calculator </h1>
       
+
+
     </div>
     
     // here come the components
@@ -16,3 +16,21 @@ function App() {
 }
 
 export default App;
+
+/*
+Components
+
+-Keyboard (keeps all the buttons)
+
+- Button - has onClick function for 
+    - numbers
+      * change the screen,
+        making the current nr bigger 
+    - operations
+        * changes the screen only with the sign
+        * the first number is remembered
+    - delete
+
+- Screen (gets the content from the keyboard)
+
+*/
