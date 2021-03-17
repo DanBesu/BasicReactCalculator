6f623052e118ac1f296e55828c9d6117f6858339
911_Besu_Dan <danbesu15@gmail.com>
2021-03-17 11:37:03 +0200

    all operations

--- src/App.js
+++ src/App.js
@@ -1,36 +1,127 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Keyboard from './components/Keyboard'
 import Screen from './components/Screen'
+import './App.css'
 
 function App() {
+
+  const [operand1, setOperand1] = useState(0); 
+  const [operand2, setOperand2] = useState(null);
+  const [screenText, setScreenText] = useState('0');
+  const [operation, setOperation] = useState('');
+  const [status, setStatus] = useState(1); // 1 - equal(end of operation, work with operand1), 2 - operation open (work with operand2) 
+  // let status = 1;  
+
+  useEffect( ()=> {
+    if(status === 1){
+      setScreenText(`${operand1}`);
+    }
+  }, [operand1, status])
+
+  useEffect( ()=> {
+    if(status === 2 && operand2 !== null){
+      setScreenText(`${operand2}`);
+    }
+  }, [operand2])
+
+  useEffect( ()=> {
+    setScreenText(`${operation}`);
+  }, [operation])
+
+  const displayNewDigit = (newDigit) => {
+    if(status === 1){
+      setOperand1(operand1 * 10 + newDigit)
+    }
+    if(status === 2){
+      if(operand2 === null)
+        setOperand2(newDigit)
+      else
+        setOperand2(operand2 * 10 + newDigit)
+    }
+  }
+
+  const equals = () => {
+    if(operation === '+'){
+      setStatus(1)
+      setOperand1(operand1 + operand2)
+      setOperand2(null)
+    }
+    else if(operation === '-'){
+      setStatus(1)
+      setOperand1(operand1 - operand2)
+      setOperand2(null)
+    }
+    else if(operation === '*'){
+      setStatus(1)
+      if(operand2 !== null)
+        setOperand1(operand1 * operand2)
+      setOperand2(null)
+    }
+    else if(operation === '/'){
+      setStatus(1)
+      if(operand2 !== null)
+        setOperand1(Math.floor(operand1 / operand2))
+      setOperand2(null)
+    }
+  }
+
+  const add = () => {
+    setOperation('+');
+    setStatus(2);
+    setOperand1(operand1 + operand2);
+    setOperand2(null);
+  }
+
+  const subtract = () => {
+    setOperation('-');
+    setStatus(2);
+    setOperand1(operand1 - operand2);
+    setOperand2(null);
+  }
+
+  const multiply = () => {
+    setOperation('*');
+    setStatus(2);
+    if(operand2 !== null)
+      setOperand1(operand1 * operand2);
+    setOperand2(null);
+  }
+
+  const divide = () => {
+    setOperation('/');
+    setStatus(2);
+    if(operand2 !== null)
+      setOperand1(operand1 / operand2);
+    setOperand2(null);
+  }
+
+  const reverseSign = () => status === 1 ? setOperand1(operand1 * -1) : setOperand2(operand2 * -1)
+
+  const reset = () => {setOperand2(null); setOperand1(0); setOperation('')}
+
+  const del = () => status === 1 ? setOperand1((operand1 - operand1 % 10)/10) : setOperand2((operand2 - operand2 % 10)/10)
+
+  const powerTwo = () => status === 1 ? setOperand1(operand1 ** 2) : setOperand2(operand2 ** 2)
+
   return (
     
-    <div>
+    <div className='center'>
       <h1 className='f1'> Here we have a Calculator </h1>
-      <Screen/>
-      <Keyboard/>
+      <Screen text={screenText}/>
+      <Keyboard 
+        displayNewDigit={displayNewDigit}
+        del={del}
+        reverseSign={reverseSign}
+        reset={reset}
+        equals={equals}
+        powerTwo={powerTwo}
+        add={add}
+        subtract={subtract}
+        multiply={multiply}
+        divide={divide}
+      />
     </div>
-    
-    // here come the components
   );
 }
 
 export default App;
-
-/*
-Components
-
--Keyboard (keeps all the buttons)
-
-- Button - has onClick function for 
-    - numbers
-      * change the screen,
-        making the current nr bigger 
-    - operations
-        * changes the screen only with the sign
-        * the first number is remembered
-    - delete
-
-- Screen (gets the content from the keyboard)
-
-*/
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -1,38 +1,37 @@
 import React from 'react';
 import Button from './Button';
 
-const Keyboard = () => {
+const Keyboard = ({displayNewDigit, del, reverseSign, reset, equals, powerTwo, add, subtract, multiply, divide}) => {
     return (
         <div>
             <div className='row'>
-                <Button text="del"/>
-                <Button text="1/x"/>
-                <Button text="x^2"/>
-                <Button text="/"/>
+                <Button text="del" onClick={del}/>
+                <Button text="C" onClick={reset}/>
+                <Button text="x^2" onClick={() => powerTwo()}/>
+                <Button text="/" onClick={()=>divide()}/>
             </div>
 
             <div className='row'>
-                <Button text="7"/>
-                <Button text="8"/>
-                <Button text="9"/>
-                <Button text="x"/>
+                <Button text="7" onClick={()=>displayNewDigit(7)}/>
+                <Button text="8" onClick={()=>displayNewDigit(8)}/>
+                <Button text="9" onClick={()=>displayNewDigit(9)}/>
+                <Button text="x" onClick={()=>multiply()}/>
             </div>
-                <Button text="4"/>
-                <Button text="5"/>
-                <Button text="6"/>
-                <Button text="-"/>
+                <Button text="4" onClick={()=>displayNewDigit(4)}/>
+                <Button text="5" onClick={()=>displayNewDigit(5)}/>
+                <Button text="6" onClick={()=>displayNewDigit(6)}/>
+                <Button text="-" onClick={()=>subtract()}/>
             <div className='row'>
-                <Button text="1"/>
-                <Button text="2"/>
-                <Button text="3"/>
-                <Button text="+"/>
+                <Button text="1" onClick={()=>displayNewDigit(1)}/>
+                <Button text="2" onClick={()=>displayNewDigit(2)}/>
+                <Button text="3" onClick={()=>displayNewDigit(3)}/>
+                <Button text="+" onClick={()=>add()}/>
             </div>
 
             <div className='row'>
-                <Button text="+/-"/>
-                <Button text="0"/>
-                <Button text="."/>
-                <Button text="="/>
+                <Button text="+/-" onClick={()=>reverseSign()}/>
+                <Button text="0" onClick={()=>displayNewDigit(0)}/>
+                <Button text="=" onClick={()=>equals()}/>
             </div>            
         </div>
     );
